5a4806ba891dd83dff864aaf888bf6623d476185
Jan Čelovský <celovskyhonza@gmail.com>
2023-07-20 17:31:47 +0200

    User validation

--- Zaly/Zaly/Models/User.cs
+++ Zaly/Zaly/Models/User.cs
@@ -1,8 +1,16 @@
-﻿namespace Zaly.Models {
+﻿using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+
+namespace Zaly.Models {
 	public class User {
         public int Id { get; set; }
+        [RegexStringValidator("^[A-Z][a-z]+$")]
+        [Required]
         public string Name { get; set; } = "";
+        [RegexStringValidator("^[A-Z][a-z]+$")]
+        [Required]
         public string Surname { get; set; } = "";
+        [MinLength(1)]
         public string Nickname { get; set; } = "";
         public int Points { get; set; }
         public string Password { get; set; } = "";
