bf128cc21191d52b73a2dee8f68c8bf226631522
Jan Čelovský <celovskyhonza@gmail.com>
2023-07-20 13:42:55 +0200

    Update Controllers

---
+++ Zaly/Zaly/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Zaly.Models;
+
+namespace Zaly.Controllers {
+	public class AdminController : Controller {
+		readonly private DatabaseContext _context = new();
+		public IActionResult Index() {
+			this.ViewBag.Users = _context.User.ToList();
+			return View();
+		}
+		public IActionResult Add() {
+			return View();
+		}
+		public IActionResult Delete(int Id) {
+			//TODO
+			return Redirect("Index");
+		}
+		public IActionResult Edit() {
+			return View();
+		}
+	}
+}
---
+++ Zaly/Zaly/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata;
+using Zaly.Models;
+
+namespace Zaly.Controllers {
+	public class UserController : Controller {
+		private readonly DatabaseContext _context;
+		List<User> l;
+		string errormes = "nothing";
+        public UserController() {
+			try {
+				_context = new();
+				l = _context.User.ToList();
+			} catch (Exception ex) {
+				this.errormes = ex.Message;
+			}
+		}
+        public IActionResult Index() {
+			if (l != null)
+				this.ViewBag.l = l;
+			this.ViewBag.e = errormes;
+			return View();
+		}
+		public IActionResult Snake() {
+			return View();
+		}
+		public IActionResult Leaderboard() {
+			return View();
+		}
+		public IActionResult Profile() {
+			return View();
+		}
+		public IActionResult Player() {
+			return View();
+		}
+	}
+}
