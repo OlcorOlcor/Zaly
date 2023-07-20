a7897fbf789226ea1f440e29dcfa571b41756aad
Jan Čelovský <celovskyhonza@gmail.com>
2023-07-20 13:41:43 +0200

    Add DatabaseContext class

---
+++ Zaly/Zaly/Models/DatabaseContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+
+namespace Zaly.Models {
+	public class DatabaseContext : DbContext {
+		public DbSet<User> User { get; set; }
+		public DbSet<Admin> Admin { get; set; }
+		public DbSet<Team> Team { get; set; }
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+			optionsBuilder.UseMySQL("Server=a055um.forpsi.com;Database=f170685;Uid=f170685;Pwd=Rnp2CxTP");
+		}
+	}
+}
