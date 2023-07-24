cfae109f57f9042da19948ce0a3bf4c03c2bf13a
Jan Čelovský <celovskyhonza@gmail.com>
2023-07-24 13:53:13 +0200

    Add Question class

---
+++ Zaly/Zaly/Models/Question.cs
@@ -0,0 +1,11 @@
+﻿namespace Zaly.Models {
+    public class Question {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Text { get; set; }
+        public int Points { get; set; }
+        public bool Multipart { get; set; }
+        public string Img { get; set; }
+        public string Answer { get; set; }
+    }
+}
