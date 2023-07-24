f01a1e3b1903bcbf458099a16c4ef140ddeb6f14
Jan Čelovský <celovskyhonza@gmail.com>
2023-07-24 13:56:04 +0200

    Add MultipartAnswer class

---
+++ Zaly/Zaly/Models/MultipartAnswer.cs
@@ -0,0 +1,8 @@
+﻿namespace Zaly.Models {
+    public class MultipartAnswer {
+        public int Id { get; set; }
+        public string Answer { get; set; }
+        public bool Correct { get; set; }
+        public int QuestionId { get; set; }
+    }
+}
